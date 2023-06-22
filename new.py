042b7c9a606c1f929c905cb3ced369a961a2a737
Ashwin Sunuwar <ashwinsunuwarr2@gmail.com>
2023-06-22 17:01:13 +0545

    UI first commit

--- new.py
+++ new.py
@@ -1,233 +1,156 @@
-
 import tkinter as tk
-from tkinter import messagebox
-from tkinter import filedialog
+from tkinter import ttk
 from PIL import Image, ImageTk
-import os
-import mysql.connector
-import base64
-
-
-MAX_RESOLUTION = 1080
-
 
 root = tk.Tk()
-root['bg']='#237de1'
-root.geometry("1080x750")
-root.title("Register Criminal")
+root.geometry("1190x760")
 
-connection = mysql.connector.connect(host='localhost', user='root', password='', port='3306', database='mydb')
-c= connection.cursor()
+title_color = "#6f86b9"
+root.config(bg=title_color)
+root.title("Criminal Face Recognition System")
 
+my_color = "#6f86b9"
 
-def add_front():
-    global front_file, img
-    
-    f_types = [('png files', '*.png'), ('jpg files', '*.jpg'), ('jpeg files', '*.jpeg')]
-    front_file = tk.StringVar()
-    front_file.set(filedialog.askopenfilename(filetypes=f_types))
-    
-    if front_file.get():
-        img = Image.open(front_file.get())
-        img.thumbnail((200, 200))  # Resize the image for preview
-        
-        # Create a PhotoImage object for the image
-        img = ImageTk.PhotoImage(img)
-        
-        # Configure the image_label with the selected image
-        front_image_label.configure(image=img)
-        front_image_label.image = img  # Store a reference to the image
-
-def add_left():
-    global left_file, img
-    
-    f_types = [('png files', '*.png'), ('jpg files', '*.jpg'), ('jpeg files', '*.jpeg')]
-    left_file = tk.StringVar()
-    left_file.set(filedialog.askopenfilename(filetypes=f_types))
-    
-    if left_file.get():
-        img = Image.open(left_file.get())
-        img.thumbnail((200, 200))  # Resize the image for preview
-        
-        # Create a PhotoImage object for the image
-        img = ImageTk.PhotoImage(img)
-        
-        # Configure the image_label with the selected image
-        left_image_label.configure(image=img)
-        left_image_label.image = img  # Store a reference to the image
-
-def add_right():
-    global right_file, img
-    
-    f_types = [('png files', '*.png'), ('jpg files', '*.jpg'), ('jpeg files', '*.jpeg')]
-    right_file = tk.StringVar()
-    right_file.set(filedialog.askopenfilename(filetypes=f_types))
-    
-    if right_file.get():
-        img = Image.open(right_file.get())
-        img.thumbnail((200, 200))  # Resize the image for preview
-        
-        # Create a PhotoImage object for the image
-        img = ImageTk.PhotoImage(img)
-        
-        # Configure the image_label with the selected image
-        right_image_label.configure(image=img)
-        right_image_label.image = img  # Store a reference to the image
-
-
-# will be submitted to db
-def submit_form():
-    global front_file, img
-
-    #file from add_front
-    name = name_entry.get().strip()
-    father_name = father_entry.get()
-    mother_name = mother_entry.get()
-    age = age_entry.get()
-    nationality = nationality_entry.get()
-    gender = gender_entry.get().strip()
-    crime = crime_entry.get("1.0", tk.END).strip()
-    image_data = open(front_file.get(), 'rb')
-    image_data = image_data.read()
-
-    left_data = open(left_file.get(), 'rb')
-    left_data = left_data.read()
-
-    right_data = open(right_file.get(), 'rb')
-    right_data = right_data.read()
-
-
-    
-    if name == "":
-        messagebox.showerror("Error", "Please fill in the Name field.")
-    elif gender == "":
-        messagebox.showerror("Error", "Please fill in the Gender field.")
-    elif crime == "":
-        messagebox.showerror("Error", "Please fill in the Crime Committed field.")
-    else:        
-        # save the details to a database
-        data_insert = "INSERT INTO `criminal_reg`(`name`, `father_name`, `mother_name`, `age`, `gender`, `nationality`, `crime`, `front_img`, `left_img`, `right_img`) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
-        vals = (name, father_name, mother_name, age, gender, nationality, crime, image_data, left_data, right_data)
-        c.execute(data_insert, vals)
-        connection.commit()
-        
-        # clears fields
-        name_entry.delete(0, tk.END)
-        father_entry.delete(0, tk.END)
-        mother_entry.delete(0, tk.END)
-        age_entry.delete(0, tk.END)
-        gender_entry.delete(0, tk.END)
-        nationality_entry.delete(0, tk.END)
-        crime_entry.delete("1.0", tk.END)
-        image_data = None
-        right_data = None
-        left_data = None
-        
-        messagebox.showinfo("Success", f"Criminal with name: {name} has been registered.")
-
-
-frame = tk.Frame(root, bg="#237de1")
-frame.pack(pady=20, expand=True, anchor='center')
-
-
-#name label
-name_label = tk.Label(frame, bg="#237de1")
-name_label.grid(row=0, column=0, padx=10, pady=10, sticky="e")
-
-asterisk_label = tk.Label(name_label, text="*", font=('Courier New', 18, 'bold'), fg='red', bg="#237de1")
-asterisk_label.grid(row=0, column=0)
-
-text_label = tk.Label(name_label, text="Name:", font=('Courier New', 18, 'bold'), bg="#237de1", fg="orange")
-text_label.grid(row=0, column=1)
-
-name_entry = tk.Entry(frame, width=32, font=('Courier New', 16))
-name_entry.grid(row=0, column=1, padx=10, pady=10)
-
-#father name
-father_label = tk.Label(frame, fg='orange', text="Father's Name:", font=('Courier New', 18, 'bold'), bg="#237de1")
-father_label.grid(row=1, column=0, padx=10, pady=10, sticky="e")
-father_entry = tk.Entry(frame, width=32, font=('Courier New', 16))
-father_entry.grid(row=1, column=1, padx=10, pady=10)
-
-#mother name
-mother_label = tk.Label(frame, fg='orange', text="Mother's Name:", font=('Courier New', 18, 'bold'), bg="#237de1")
-mother_label.grid(row=2, column=0, padx=10, pady=10, sticky="e")
-mother_entry = tk.Entry(frame, width=32, font=('Courier New', 16))
-mother_entry.grid(row=2, column=1, padx=10, pady=10)
-
-#age
-age_label = tk.Label(frame, fg='orange', text="Age:", font=('Courier New', 18, 'bold'), bg="#237de1")
-age_label.grid(row=3, column=0, padx=10, pady=10, sticky="e")
-age_entry = tk.Entry(frame, width=32, font=('Courier New', 16))
-age_entry.grid(row=3, column=1, padx=10, pady=10)
-
-#gender
-gender_label = tk.Label(frame, bg="#237de1")
-gender_label.grid(row=4, column=0, padx=10, pady=10, sticky="e")
-
-asterisk_label = tk.Label(gender_label, text="*", font=('Courier New', 18, 'bold'), fg='red', bg="#237de1")
-asterisk_label.grid(row=0, column=0)
-
-text_label = tk.Label(gender_label, text="Gender:", font=('Courier New', 18, 'bold'), bg="#237de1", fg="orange")
-text_label.grid(row=0, column=1)
-
-gender_entry = tk.Entry(frame, width=32, font=('Courier New', 16))
-gender_entry.grid(row=4, column=1, padx=10, pady=10)
-
-#nationality
-nationality_label = tk.Label(frame, bg="#237de1")
-nationality_label.grid(row=5, column=0, padx=10, pady=10, sticky="e")
-
-asterisk_label = tk.Label(nationality_label, text="*", font=('Courier New', 18, 'bold'), fg='red', bg="#237de1")
-asterisk_label.grid(row=0, column=0)
-
-text_label = tk.Label(nationality_label, text="Nationality:", font=('Courier New', 18, 'bold'), bg="#237de1", fg="orange")
-text_label.grid(row=0, column=1)
-
-nationality_entry = tk.Entry(frame, width=32, font=('Courier New', 16))
-nationality_entry.grid(row=5, column=1, padx=10, pady=10)
-
-#crime
-crime_label = tk.Label(frame, bg="#237de1")
-crime_label.grid(row=6, column=0, padx=10, pady=10, sticky="e")
-
-asterisk_label = tk.Label(crime_label, text="*", font=('Courier New', 18, 'bold'), fg='red', bg="#237de1")
-asterisk_label.grid(row=0, column=0)
-
-text_label = tk.Label(crime_label, text="Crime Committed:", font=('Courier New', 18, 'bold'), bg="#237de1", fg="orange")
-text_label.grid(row=0, column=1)
-
-crime_entry = tk.Text(frame, width=32, height=1, font=('Courier New', 16))
-crime_entry.grid(row=6, column=1, padx=10, pady=10)
+style = ttk.Style()
+style.configure('TRoundedFrame.TFrame', background=my_color, borderwidth=5, relief='flat')
 
 
 
 
-#front_image btn
-browse_btn = tk.Button(frame, text="Select Front Face Image", font=('Courier New', 13), command=lambda:add_front())
-browse_btn.grid(row=7, column=0, padx=4, pady=4)
+def animate_gif():
+    global frame_index
 
-front_image_label = tk.Label(frame)
-front_image_label.grid(row=9, column=0)
+    frame = gif_frames[frame_index]
+    disp_frame = ImageTk.PhotoImage(frame)
+    lbl_icon.configure(image=disp_frame)
+    lbl_icon.image = disp_frame
 
-# left image btn
-browse_btn = tk.Button(frame, text="Select Left Face Image", font=('Courier New', 13), command=lambda:add_left())
-browse_btn.grid(row=7, column=1, padx=4, pady=4)
+    frame_index = (frame_index + 1) % len(gif_frames)
+    root.after(100, animate_gif)
 
-left_image_label = tk.Label(frame)
-left_image_label.grid(row=9, column=1)
+# Load the animated GIF frames
+gif = Image.open("icon_img/eye.gif")
+gif_frames = []
+try:
+    while True:
+        frame = gif.copy()
+        frame.thumbnail((250, 250))
+        gif_frames.append(frame)
+        # Seek next frame
+        gif.seek(len(gif_frames))
+except EOFError:
+    pass
 
+frame_index = 0
 
-#right image btn
-browse_btn = tk.Button(frame, text="Select Right Face Image", font=('Courier New', 13), command=lambda:add_right())
-browse_btn.grid(row=7, column=2, padx=4, pady=4)
+lbl_title = tk.Label(root, text="CRIMINAL FACE RECOGNITION SYSTEM", font=("Courier new", 24, 'bold'), justify="right", bg=title_color)
+lbl_title.pack(pady=7, ipadx=5, ipady=5)
 
-right_image_label = tk.Label(frame)
-right_image_label.grid(row=9, column=2)
+#canvas 
+canvas = tk.Canvas(root, bg=my_color)
+canvas.pack(fill='both', expand=True, ipadx=40, ipady=0)
 
-#confirm button 
-confirm_btn = tk.Button(root, text="Confirm", font=('Courier New', 16, 'bold'), command=submit_form, bg="#feac57")
-confirm_btn.pack(pady=10)
+#frame 1
+frm1 = ttk.Frame(canvas, style='TRoundedFrame.TFrame')
+frm1.pack(pady=4, ipady=40)
 
+lbl_icon = tk.Label(frm1, bg=my_color)
+lbl_icon.pack(padx=5, pady=7)
+
+lbl_details = tk.Label(frm1, text="System that detects human faces from live footage and recognize them.\nBetter than other biometric scanners as it doesnot requires physical presence.",
+                       font=("Verdana", 18, 'italic'), bg=my_color)
+lbl_details.pack(pady=16, padx=5, ipadx=50, ipady=40)
+
+#frame 2
+frm2 = tk.Frame(frm1)
+frm2.pack(padx=10, pady=50)
+frm2.configure(background=my_color)
+
+btn_login = tk.Button(frm2, text="LOGIN", width=14 , font=("Verdana", 16), relief="flat", bg="#e13746")
+btn_login.pack(side='left', padx=(0, 20), pady=10)
+
+btn_signup = tk.Button(frm2, text="SIGN UP", width=14, font=("Verdana", 16), relief="flat", bg="#432771")
+btn_signup.pack(side='right', padx=(20, 0), pady=10)
+
+btn_learn_more = tk.Button(frm1, text="Learn More.", font=("Verdana", 9, "italic"), relief="flat", bg=my_color)
+btn_learn_more.pack(pady=10)
+
+animate_gif()
 
 root.mainloop()
+
+
+# import tkinter as tk
+# from tkinter import ttk
+# from PIL import Image, ImageTk
+
+# root = tk.Tk()
+# root.geometry("1190x860")
+# root.config(bg="#728a75")
+# root.title("Criminal Face Recognition System")
+
+# style = ttk.Style()
+# style.configure('TRoundedFrame.TFrame', background='orange', borderwidth=5, relief='groove', bordercolor='#625e75')
+
+# def animate_gif():
+#     global frame_index
+
+#     frame = gif_frames[frame_index]
+#     disp_frame = ImageTk.PhotoImage(frame)
+#     lbl_icon.configure(image=disp_frame)
+#     lbl_icon.image = disp_frame
+
+#     frame_index = (frame_index + 1) % len(gif_frames)
+#     root.after(100, animate_gif)
+
+# # Load the animated GIF frames
+# gif = Image.open("icon_img/eye.gif")
+# gif_frames = []
+# try:
+#     while True:
+#         frame = gif.copy()
+#         frame.thumbnail((250, 250))
+#         gif_frames.append(frame)
+#         # Seek next frame
+#         gif.seek(len(gif_frames))
+# except EOFError:
+#     pass
+
+# frame_index = 0
+
+# # #canvas 
+# # canvas = tk.Canvas(root, bg='#728a75')
+# # canvas.pack(fill='both', expand=True, pady=10)
+
+# #frame 1
+# frm1 = ttk.Frame(root, style='TRoundedFrame.TFrame')
+# frm1.pack()
+
+# lbl_title = tk.Label(frm1, text="CRIMINAL FACE RECOGNITION SYSTEM", font=("Courier new", 24, 'bold'), bg="#728a75")
+# lbl_title.pack(ipadx=5, ipady=5)
+
+
+# lbl_icon = tk.Label(frm1, bg="white")
+# lbl_icon.pack(padx=5, pady=7)
+
+# lbl_details = tk.Label(frm1, text="System that detects human faces from live footage and recognize them.\nBetter than other biometric scanners as it doesnot requires physical presence.",
+#                        font=("Verdana", 18, 'italic'), bg="white")
+# lbl_details.pack(pady=16, padx=5, ipadx=50, ipady=40)
+
+# #frame 2
+# frm2 = tk.Frame(frm1)
+# frm2.pack(padx=10, pady=50)
+# frm2.configure(background="#625e75")
+
+# btn_login = tk.Button(frm2, text="LOGIN", font=("Verdana", 18, "bold"), relief="sunken", bg="red")
+# btn_login.pack(side='left', padx=(0, 20), pady=10)
+
+# btn_signup = tk.Button(frm2, text="SIGN UP", font=("Verdana", 18, "bold"), relief="sunken", bg="purple")
+# btn_signup.pack(side='right', padx=(20, 0), pady=10)
+
+# btn_learn_more = tk.Button(frm1, text="Learn More.", font=("Verdana", 9, "italic"), relief="flat", bg="white")
+# btn_learn_more.pack(pady=10)
+
+# animate_gif()
+
+# root.mainloop()
